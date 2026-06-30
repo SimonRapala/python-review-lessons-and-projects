ce697eb604a2b1f73c11a8afea2af6c35a6d925b
SimonRapala <simonrapala833@gmail.com>
2026-06-30 11:06:36 -0400

    Covered user input, similar to C the main difference is just how simple the yntax is and how flexable it is

---
+++ 3_input.py
@@ -0,0 +1,19 @@
+# name = input("What is you name: ")
+# age = int(input("How old are you: "))
+
+# print(f"Hello {name}")
+# print(f"You are {age + 1} years old - 1")
+
+# print(type(name))
+# print(type(age))
+
+# length = float(input("Length(cm): "))
+# width = float(input("Width(cm): "))
+
+# print(f"Total Area: {length * width}cm²")
+
+item = input("What do you want to buy: ")
+price = float(input("What is price: "))
+quantity = int(input("How many: "))
+
+print(f"Item: {item}, Amount: {quantity}, Total Cost: {quantity * price}")
