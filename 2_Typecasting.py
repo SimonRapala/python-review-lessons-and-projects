8f0452fed80afdf2237299ba6e0212a999dd1ce1
SimonRapala <simonrapala833@gmail.com>
2026-06-30 10:51:13 -0400

    Learned about type castingwhich is kind of new as we dont really do it in C

---
+++ 2_Typecasting.py
@@ -0,0 +1,22 @@
+#Converting variable to diff data types
+
+name = "Simon Rapala"
+age = 21
+gpa = 3.9
+isStudent = True
+name2 = ""
+
+print(type(age))
+
+print(int(gpa))
+
+print(gpa)
+
+print(str(age))
+print(type(str(age)))
+
+#print(str(age) + 1) bad
+print(str(age) + "1")
+
+print(bool(name))
+print(bool(name2))
