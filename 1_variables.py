8ddadf4df1a4dfca90a476bee28d5ee841c1e201
SimonRapala <simonrapala833@gmail.com>
2026-06-29 23:03:12 -0400

    First lesson done on the basic variable types, same as C but syntax is different and much easier especially with strings

---
+++ 1_variables.py
@@ -0,0 +1,28 @@
+#Strings - chars making a word or sentence
+firstName = "Simon Rapala"
+favFood = "Sliders"
+print(firstName)
+print(f"Hello {firstName}")
+print(f"My favourite food is {favFood}")
+
+#Integers - whole numbers
+#age = "25" bad
+age = 25 #good
+numStudents = 35
+
+print(f"I am {age} years old")
+print(f"I am {age} years old and have {numStudents} in my class")
+
+#Float - number with decimal portion
+GPA = 3.9
+print(f"My current GPA is {GPA}")
+
+#Boolean - true or false flag
+isStudent = True
+isParent = False
+isSale = False
+print(f"I am a student: {isStudent}. I am a parent: {isParent}")
+if isSale:
+  print("Item is on sale")
+else:
+  print("Item is not on sale")
