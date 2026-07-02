e72bffbf41a9eef8a667ecbe581d65b893b4519e
SimonRapala <simonrapala833@gmail.com>
2026-07-02 14:04:36 -0400

    Learned about the ternary operator, I know it exists in C however I have never used it, so this was new for me

---
+++ 7_conditionals.py
@@ -0,0 +1,12 @@
+num = 5
+
+print("Positive" if num > 0 else "Negative")
+
+num2 = 7
+result = "EVEN" if num2 %2 == 0 else "NEGATIVE"
+print(result)
+
+a = 6
+b = 7
+maxNum = a if a > b else b
+print(maxNum)
