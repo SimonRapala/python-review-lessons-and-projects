0c03f65a3070020d8b276288ef3055aea69037d8
SimonRapala <simonrapala833@gmail.com>
2026-07-03 00:49:01 -0400

    A lot to remember but will be useful for QoL

---
+++ 10_formatSpecifiers.py
@@ -0,0 +1,11 @@
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+print(f"Price 1 is ${price1:.3f}")
+print(f"Price 2 is ${price2:.3f}")
+print(f"Price 3 is ${price3:.3f}")
+
+print(f"Price 1 is ${price1:10}")
+print(f"Price 1 is ${price1:020}")
+
+print(f"Price 1 is ${price2: .2f}")
