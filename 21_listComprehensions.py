acca8674d59cf2b842a23a671245419a3de2baed
SimonRapala <simonrapala833@gmail.com>
2026-07-05 22:03:28 -0400

    This is new but I dont see it being extremely useful in general but can be good for niche situations

--- 21_listComprehensions.py
+++ 21_listComprehensions.py
@@ -15,3 +15,19 @@ print(doubles)
 doubles = [x * 2 for x in range(0,11)]
 
 print(doubles)
+
+fruits = ["apple", "orange", "banana"]
+fruitsUpper = [fruit.upper() for fruit in fruits]
+fruitsCap = [fruit.capitalize() for fruit in fruits]
+
+print(fruits)
+print(fruitsUpper)
+print(fruitsCap)
+
+numbers = [x for x in range(0, 11)]
+print(numbers)
+evenNum = [x for x in numbers if x % 2 == 0]
+oddNum = [x for x in numbers if x % 2 == 1 ]
+
+print(evenNum)
+print(oddNum)
