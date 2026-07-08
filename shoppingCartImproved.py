0bf13f606d783a0436275362a0e8a6c1ff28f148
Simon Rapala Dell <simonrapala833@gmail.com>
2026-07-07 20:27:21 -0400

    Started a shopping cart project to put to use the skills I have learned. For now just started with the simple flow and displaying the options along with normalization of inputs.

---
+++ shoppingCartImproved.py
@@ -0,0 +1,69 @@
+groceryCart = []
+groceryList = {
+    "produce": [
+        {"item": "Bananas", "quantity": 5, "price": 2.50},
+        {"item": "Spinach", "quantity": 2, "price": 3.99},
+        {"item": "Avocados", "quantity": 3, "price": 1.50}
+    ],
+    "dairy and eggs": [
+        {"item": "Milk", "quantity": 7, "price": 5.49},
+        {"item": "Eggs", "quantity": 3, "price": 3.29},
+        {"item": "Greek Yogurt", "quantity": 2, "price": 4.99}
+    ],
+    "meat and protein": [
+        {"item": "Chicken Breasts", "quantity": 2, "price": 9.99},
+        {"item": "Tofu", "quantity": 2, "price": 2.29},
+        {"item": "Turkey", "quantity": 1, "price": 10.99}
+    ],
+    "pantry staples": [
+        {"item": "Quinoa", "quantity": 3, "price": 4.50},
+        {"item": "Olive Oil", "quantity": 4, "price": 12.99},
+        {"item": "Butter", "quantity": 5, "price": 5.99 }
+    ]
+}
+
+def normalizeAisleInput(shopperChoice):
+    if shopperChoice == "dairy" or "dairy and" or "eggs" or "and eggs" or "dairy and eggs":
+        return "dairy and eggs"
+    elif shopperChoice == "meat" or "meat and" or "and protein" or "protein" or "meat and protein":
+        return "meat and protein"
+    elif shopperChoice == "pantry" or "staples" or "pantry staples":
+        return "pantry staples"
+    elif shopperChoice == "produce":
+        return "produce"
+    else:
+        return "No"
+
+
+
+def main():
+    shopperName = input("What Is Your Name: ")
+    wallet = int(input("How Much Money Do You Have Today:" ))
+
+    firstPass = True
+    while True:
+        if firstPass is True:
+            firstPass = False
+            print("Where Would You like To Start Today?")
+            aisle = input("Produce | Dairy and Eggs | Meat and Protein | Pantry Staples | (q to leave)\n").lower().strip()
+
+            if aisle == "q":
+                break
+            else:
+                aisle = normalizeAisleInput(aisle)
+
+                if aisle == "No":
+                    print("Not A Valid Input!!!")
+                    pass
+                else:
+                    aisleList = groceryList.get(aisle)
+                    for item in aisleList:
+                        print("-------------")
+                        for attribute in item:
+                            print(f"{attribute}: {item.get(attribute)}", end=" ")
+                            print("\n-------------")
+                        print()
+
+
+if __name__ == "__main__":
+    main()
